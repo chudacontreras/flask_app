913ebc8361342148bc7e204e657fd830f9b79483
chuda <chudacontreras@gmail.com>
2020-04-27 22:31:40 -0400

    jugando con flask

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+todos = ['TODO 1', 'TODO 2', 'TODO 3']
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
@@ -15,5 +17,10 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
+    
+    context = {
+        'user_ip': user_ip,
+        'todos': todos,
+    }
 
-    return 'Hello World Flask Chuda, tu IP es: {}'.format(user_ip)
+    return render_template('hello.html', **context)
