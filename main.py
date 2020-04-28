d184a95351c9d9eeb803c721bfce14a90fc5d954
chuda <chudacontreras@gmail.com>
2020-04-27 21:33:10 -0400

    primer commit configuraciones de la app

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, make_response, redirect, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+
+    return 'Hello World Flask Chuda, tu IP es: {}'.format(user_ip)
