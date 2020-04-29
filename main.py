28f5a03ffadfe72ada9a881cb22d877c3284a1e5
chuda <chudacontreras@gmail.com>
2020-04-29 15:32:39 -0400

    agregando blueprint

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+from app import create_app
+from app.forms import LoginForm
 
-app.config['SECRET_KEY'] = 'SUPER SECRETO'
+app = create_app()
 
 
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
 
 
 @app.cli.command()
@@ -30,6 +20,7 @@ def test():
 def not_found(error):
     return render_template('404.html', error=error)
 
+
 @app.errorhandler(500)
 def server_error(error):
     return render_template('500.html', error=error)
@@ -40,31 +31,21 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    #response.set_cookie('user_ip', user_ip)
     session['user_ip'] = user_ip
 
     return response
 
 
-@app.route('/hello', methods=['GET', 'POST'])
+@app.route('/hello', methods=['GET'])
 def hello():
     user_ip = session.get('user_ip')
-    #request.cookies.get('user_ip')
-    login_form = LoginForm()
     username = session.get('username')
-    
+
     context = {
         'user_ip': user_ip,
         'todos': todos,
-        'login_form': login_form,
         'username': username
     }
-    if login_form.validate_on_submit():
-        username = login_form.username.data
-        session['username'] = username
-
-        flash('Nombre de usario registrado con éxito!')
-
-        return redirect(url_for('index'))
 
     return render_template('hello.html', **context)
+
