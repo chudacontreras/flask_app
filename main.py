a2c65ed22d33ac39ae8a98d22ed75c2bdaa7e032
chuda <chudacontreras@gmail.com>
2020-04-30 17:18:55 -0400

    agregando validacion de usuarios

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
+from flask import request, make_response, redirect, render_template, session, url_for, flash
+from flask_login import login_required, current_user
+
 
 from app import create_app
 from app.forms import LoginForm
@@ -35,21 +37,16 @@ def index():
 
 
 @app.route('/hello', methods=['GET'])
+@login_required
 def hello():
     user_ip = session.get('user_ip')
-    username = session.get('username')
+    username = current_user.id
 
     context = {
         'user_ip': user_ip,
         'todos': get_todos(user_id=username),
         'username': username
     }
-
-    users = get_users()
-
-    for user in users:
-        print(user.id)
-        print(user.to_dict()['password'])
 
     return render_template('hello.html', **context)
 
